595322a06221e05164b891a82d82688070ac27e9
Assailent Dev <Jamie.hanson@education.nsw.gov.au>
2024-08-29 20:15:25 +1000

    Updated

--- MaccasRun/Assets/Scripts/InputManager.cs
+++ MaccasRun/Assets/Scripts/InputManager.cs
@@ -5,39 +5,46 @@ using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-
     public static PlayerInput PlayerInput;
 
     public static Vector2 Movement;
     public static bool JumpWasPressed;
     public static bool JumpIsHeld;
-    public static bool jumpWasReleased;
+    public static bool JumpWasReleased;
     public static bool RunIsHeld;
+    public static bool DashWasPressed;
+    public static bool TestWasPressed;
 
     private InputAction _moveAction;
     private InputAction _jumpAction;
     private InputAction _runAction;
+    private InputAction _dashAction;
+    private InputAction _testAction;
 
-
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         PlayerInput = GetComponent<PlayerInput>();
 
         _moveAction = PlayerInput.actions["Move"];
         _jumpAction = PlayerInput.actions["Jump"];
         _runAction = PlayerInput.actions["Run"];
+        _dashAction = PlayerInput.actions["Dash"];
+
+        _testAction = PlayerInput.actions["Test"];
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         Movement = _moveAction.ReadValue<Vector2>();
 
         JumpWasPressed = _jumpAction.WasPressedThisFrame();
         JumpIsHeld = _jumpAction.IsPressed();
-        jumpWasReleased = _jumpAction.WasReleasedThisFrame();
+        JumpWasReleased = _jumpAction.WasReleasedThisFrame();
 
         RunIsHeld = _runAction.IsPressed();
+
+        DashWasPressed = _dashAction.WasPressedThisFrame();
+
+        TestWasPressed = _testAction.WasPressedThisFrame();
     }
 }
